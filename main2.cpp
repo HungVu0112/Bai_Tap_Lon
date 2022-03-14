193746e074a12b90005361c04524c8925becec6d
HungVu0112 <pcsolaggy123@gmail.com>
2022-03-14 07:33:26 +0700

    Third commit

---
+++ main2.cpp
@@ -0,0 +1,47 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <iostream>
+
+#define SCREEN_WIDTH  1270
+#define SCREEN_HEIGHT  700
+
+int main(int argc, char** argv){
+    if(SDL_Init(SDL_INIT_VIDEO) < 0){
+        printf("Error: SDL failed to initialize\nSDL Error: '%s'\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("First Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+    if(!window){
+        printf("Error: Failed to open window\nSDL Error: '%s'\n", SDL_GetError());
+        return 1;
+    }
+
+
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if(!renderer){
+        printf("Error: Failed to create renderer\nSDL Error: '%s'\n", SDL_GetError());
+        return 1;
+    }
+
+    bool running = true;
+    while(running){
+        SDL_Event event;
+        while(SDL_PollEvent(&event)){
+            switch(event.type){
+                case SDL_QUIT:
+                    running = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
+        SDL_RenderClear(renderer); 
+        SDL_RenderPresent(renderer);
+    }
+
+    return 0;
+}
